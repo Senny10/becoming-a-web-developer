cbaec78b40ed15e830566aa85271f4c51310cd81
Seneca Saunders <seneca.saunders@madetech.com>
2023-09-25 15:59:57 +0100

    update for accessing static files with middleware

--- 03-server-side-rendering/index.js
+++ 03-server-side-rendering/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
 const app = express();
-const port = 8000;
+const path = require("path")
+const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "./public")))
+
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
 });
 
 app.listen(port, () => {
