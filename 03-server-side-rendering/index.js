ab8cd87921acd3cfd190b0154b4ba739fa4edc96
Seneca Saunders <seneca.saunders@madetech.com>
2023-09-29 15:12:59 +0100

    update dependencies

--- 03-server-side-rendering/index.js
+++ 03-server-side-rendering/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+const {getTodo} = require("./todos")
 const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "./public")))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"))
