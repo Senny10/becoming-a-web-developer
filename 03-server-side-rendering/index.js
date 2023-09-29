e8b7debf3d0b7b4f432fdafcff5503aa3f07bc4f
Seneca Saunders <seneca.saunders@madetech.com>
2023-09-29 16:51:31 +0100

    WiP

--- 03-server-side-rendering/index.js
+++ 03-server-side-rendering/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path")
-const {getTodo} = require("./todos")
 const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("^/$|/index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"))
-});
+app.use("^/$|/index(.html)?", require("./routes/root"))
+// app.use("/todos", require("./routes/api/todolists"))
+// app.use("/users", require("./routes/api/users"))
 
 app.listen(port, () => {
   console.log(`Server Side Rendering app listening on port ${port}`);
