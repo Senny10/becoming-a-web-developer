6e2b792a782f270f17917e904fd36c8aff6b7dd5
Sen <65850511+Senny10@users.noreply.github.com>
2023-11-03 14:29:48 +0000

    reformat sql to seed data independently

--- 04-using-a-database/migrations.sql
+++ 04-using-a-database/migrations.sql
@@ -1,110 +1,14 @@
 -- Add your SQL statements in here
-
-CREATE TABLE LISTS (
-    ID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
-    URL_ID TEXT NOT NULL,
-    NAME TEXT NOT NULL
+CREATE TABLE lists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    url_id TEXT NOT NULL,
+    name TEXT NOT NULL
 );
-
 CREATE TABLE TODOS (
-    ID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
-    LIST_ID INTEGER NOT NULL,
-    TASK TEXT NOT NULL,
-    COMPLETE BOOLEAN,
-    FOREIGN KEY (LIST_ID) REFERENCES LISTS (ID) ON DELETE RESTRICT
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    list_id INTEGER NOT NULL,
+    task TEXT NOT NULL,
+    complete BOOLEAN,
+    FOREIGN KEY (list_id) REFERENCES lists (id) ON DELETE RESTRICT
 );
-
-CREATE UNIQUE INDEX UIDX_LID
-ON LISTS (URL_ID);
-
-INSERT INTO LISTS (
-    ID,
-    URL_ID,
-    NAME
-) VALUES (
-    1,
-    'default',
-    'Default'
-);
-
-INSERT INTO LISTS (
-    ID,
-    URL_ID,
-    NAME
-) VALUES (
-    2,
-    'shopping',
-    'Shopping List'
-);
-
-INSERT INTO TODOS (
-    ID,
-    LIST_ID,
-    TASK,
-    COMPLETE
-) VALUES (
-    1,
-    1,
-    'Learn some HTML',
-    TRUE
-);
-
-INSERT INTO TODOS (
-    ID,
-    LIST_ID,
-    TASK,
-    COMPLETE
-) VALUES (
-    2,
-    1,
-    'Learn some CSS',
-    TRUE
-);
-
-INSERT INTO TODOS (
-    ID,
-    LIST_ID,
-    TASK,
-    COMPLETE
-) VALUES (
-    3,
-    1,
-    'Become a full fullstack developer',
-    FALSE
-);
-
-INSERT INTO TODOS (
-    ID,
-    LIST_ID,
-    TASK,
-    COMPLETE
-) VALUES (
-    4,
-    2,
-    'Buy some milk',
-    FALSE
-);
-
-INSERT INTO TODOS (
-    ID,
-    LIST_ID,
-    TASK,
-    COMPLETE
-) VALUES (
-    5,
-    2,
-    'Buy some bananas',
-    FALSE
-);
-
-INSERT INTO TODOS (
-    ID,
-    LIST_ID,
-    TASK,
-    COMPLETE
-) VALUES (
-    6,
-    2,
-    'Buy some red velvet cake',
-    FALSE
-);
+CREATE UNIQUE INDEX uidx_lid ON lists (url_id);
---
+++ 04-using-a-database/seedData.sql
@@ -0,0 +1,31 @@
+INSERT INTO LISTS (URL_ID, NAME)
+VALUES ('default', 'Default');
+INSERT INTO LISTS (URL_ID, NAME)
+VALUES (2, 'shopping', 'Shopping List');
+INSERT INTO TODOS (ID, LIST_ID, TASK, COMPLETE)
+VALUES (1, 1, 'Learn some HTML', TRUE);
+INSERT INTO TODOS (ID, LIST_ID, TASK, COMPLETE)
+VALUES (2, 1, 'Learn some CSS', TRUE);
+INSERT INTO TODOS (ID, LIST_ID, TASK, COMPLETE)
+VALUES (
+        3,
+        1,
+        'Become a full fullstack developer',
+        FALSE
+    );
+INSERT INTO TODOS (ID, LIST_ID, TASK, COMPLETE)
+VALUES (4, 2, 'Buy some milk', FALSE);
+INSERT INTO TODOS (ID, LIST_ID, TASK, COMPLETE)
+VALUES (
+        5,
+        2,
+        'Buy some bananas',
+        FALSE
+    );
+INSERT INTO TODOS (ID, LIST_ID, TASK, COMPLETE)
+VALUES (
+        6,
+        2,
+        'Buy some red velvet cake',
+        FALSE
+    );
