e07a1b4bd77faa86bd51c702d5a52fff409ea65d
Sen <65850511+Senny10@users.noreply.github.com>
2023-12-01 22:36:53 +0000

    Refactor Lists controller functions

--- 05-building-an-api/controllers/Lists.js
+++ 05-building-an-api/controllers/Lists.js
@@ -1,10 +1,42 @@
 const getConnection = require("../config/db");
 
 async function getLists(req, res) {
-    return await getConnection().then(async (db) => {
-        db.all("SELECT * FROM lists").catch((err) => {
-            console.log(err);
-            res.status(500).json({ error: err });
-        });
-    });
+	return await getConnection().then(async (db) => {
+		db.all("SELECT * FROM lists").catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err });
+		});
+	});
 }
+async function getListByUrlId(req, res, urlId) {
+	return await getConnection().then(async (db) => {
+		db.get("SELECT * FROM lists WHERE id = ?", [urlId]).catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err });
+		});
+	});
+}
+
+async function createList(req, res, urlId) {
+	return await getConnection().then(async (db) => {
+		db.run("INSERT INTO lists (url_id, name) VALUES (?, ?)", [
+			urlId,
+			urlId.charAt(0).toUpperCase() + list.slice(1),
+		]).catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err });
+		});
+	});
+}
+
+async function updateList(req, res, urlId, id) {
+	return await getConnection().then(async (db) => {
+		db.run("UPDATE lists SET name = ? WHERE id = ?", [urlId, id]).catch(
+			(err) => {
+				console.log(err);
+				res.status(500).json({ error: err });
+			}
+		);
+	});
+}
+module.exports = { getLists, getListByUrlId, createList, updateList };
