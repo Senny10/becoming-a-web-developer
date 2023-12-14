2cdf10a457c37f9b395c03bed47f596d670f9ec9
Sen <65850511+Senny10@users.noreply.github.com>
2023-12-14 12:54:34 +0000

    Add pagination and update createList function

--- 05-building-an-api/controllers/listsController.js
+++ 05-building-an-api/controllers/listsController.js
@@ -1,34 +1,62 @@
 const getConnection = require("../config/db");
 
-async function getLists(req, res) {
-	return await getConnection().then(async (db) => {
-		db.all("SELECT * FROM lists").catch((err) => {
-			console.log(err);
+// 3. Add pagination to this endpoint
+// /api/lists?page=1 - return the second page of results
+// total - is the total number of rows in the lists table
+// perPage - the number of results per page
+function getLists(req, res) {
+	getConnection()
+		.then((db) => {
+			db.all("SELECT * FROM lists")
+				.then(lists => {
+					res.json({
+						lists,
+						meta: {
+							pagination: {
+								current: 0,
+								total: 32,
+								perPage: 10,
+							},
+						},
+					});
+				});
+		})
+		.catch((err) => {
+			console.error(err);
 			res.status(500).json({ error: err });
 		});
-	});
-}
-async function getListByUrlId(req, res) {
-	return await getConnection().then(async (db) => {
-		db.get("SELECT * FROM lists WHERE id = ?", [req.params.listId]).catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err });
-		});
-	});
-}
-
-async function createList(req, res) {
-	return await getConnection().then(async (db) => {
-		db.run("INSERT INTO lists (url_id, name) VALUES (?, ?)", [
-			req.params.listId,
-			req.params.listId.charAt(0).toUpperCase() + req.params.listId.slice(1),
-		]).catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err });
-		});
-	});
 }
 
+function createList(req, res) {
+	getConnection()
+		.then((db) => {
+			const list = req.body
+
+			db.run("INSERT INTO lists (url_id, name, user_id) VALUES (?, ?, ?)", [
+				list.listId,
+				list.listId.charAt(0).toUpperCase() + list.listId.slice(1),
+				1,
+			])
+			.then(() => {
+				res.status(201).json({
+					meta: {
+						links: {
+							update: `PATCH /api/lists/${list.listId}`,
+							delete: `DELETE /api/lists/${list.listId}`,
+						},
+					},
+				})
+			})
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err });
+		});
+}
+
+// 1. Turn this into a non-async function
+// respond with the updated list
+// 200
 async function updateList(req, res) {
 	return await getConnection().then(async (db) => {
 		db.run("UPDATE lists SET name = ? WHERE id = ?", [urlId, id]).catch(
@@ -40,6 +68,9 @@ async function updateList(req, res) {
 	});
 }
 
+// 2. Turn this into a non-async function
+// respond with nothing
+// 204
 async function deleteList(req, res, id) {
 	return await getConnection().then(async (db) => {
 		db.run("DELETE FROM lists WHERE id = ?", [id]).catch((err) => {
@@ -48,4 +79,5 @@ async function deleteList(req, res, id) {
 		});
 	});
 }
-module.exports = { getLists, getListByUrlId, createList, updateList, deleteList };
+
+module.exports = { getLists, createList, updateList, deleteList };
