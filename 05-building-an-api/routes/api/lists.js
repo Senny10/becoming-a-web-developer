6757d0db55a246fcc4899a3ec95fe8157f921e88
Sen <65850511+Senny10@users.noreply.github.com>
2023-12-14 10:38:06 +0000

    Refactor API routes and add listController

--- 05-building-an-api/routes/api/lists.js
+++ 05-building-an-api/routes/api/lists.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const listController = require("../../controllers/listController");
 
-
-router.route('/api/lists').get().post();
-router.route('/api/lists/:urlId').patch().delete();
+router
+	.route("/api/lists")
+	.get(listController.getLists)
+	.post(listController.createList);
+router
+	.route("/api/lists/:urlId")
+	.get(listController.getListByUrlId)
+	.patch(listController.updateList)
+	.delete(listController.deleteList);
 
 module.exports = router;
