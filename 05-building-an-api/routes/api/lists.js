58f4343d153afc93935771fb1a2f539ba03439b7
Sen <65850511+Senny10@users.noreply.github.com>
2023-12-14 12:54:26 +0000

    Update listController import and route parameter name

--- 05-building-an-api/routes/api/lists.js
+++ 05-building-an-api/routes/api/lists.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const listController = require("../../controllers/listController");
+const listController = require("../../controllers/listsController");
 
 router
 	.route("/api/lists")
 	.get(listController.getLists)
 	.post(listController.createList);
 router
-	.route("/api/lists/:urlId")
-	.get(listController.getListByUrlId)
+	.route("/api/lists/:listId")
 	.patch(listController.updateList)
 	.delete(listController.deleteList);
 
