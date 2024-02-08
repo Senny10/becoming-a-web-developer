6c5e8961d6120f2d674cbb00e61c8d1bc83d7290
Sen <65850511+Senny10@users.noreply.github.com>
2024-02-08 18:39:35 +0000

    Remove unnecessary code in server.js

--- 05-building-an-api/server.js
+++ 05-building-an-api/server.js
@@ -17,37 +17,25 @@ app.use("/api/user", require("./routes/api/users"));
 const listRoutes = require("./routes/api/lists");
 app.use("/api/list", listRoutes);
 
-/* get rid of this */
-function print (path, layer) {
-	if (layer.route) {
-	  layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))))
-	} else if (layer.name === 'router' && layer.handle.stack) {
-	  layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))))
-	} else if (layer.method) {
-	  console.log('%s /%s',
-		layer.method.toUpperCase(),
-		path.concat(split(layer.regexp)).filter(Boolean).join('/'))
-	}
-  }
-  
-  function split (thing) {
-	if (typeof thing === 'string') {
-	  return thing.split('/')
+function split(thing) {
+	if (typeof thing === "string") {
+		return thing.split("/");
 	} else if (thing.fast_slash) {
-	  return ''
+		return "";
 	} else {
-	  var match = thing.toString()
-		.replace('\\/?', '')
-		.replace('(?=\\/|$)', '$')
-		.match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//)
-	  return match
-		? match[1].replace(/\\(.)/g, '$1').split('/')
-		: '<complex:' + thing.toString() + '>'
+		var match = thing
+			.toString()
+			.replace("\\/?", "")
+			.replace("(?=\\/|$)", "$")
+			.match(
+				/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//
+			);
+		return match
+			? match[1].replace(/\\(.)/g, "$1").split("/")
+			: "<complex:" + thing.toString() + ">";
 	}
-  }
-  
-  app._router.stack.forEach(print.bind(null, []))
-/* get rid of this */
+}
+
 
 app.all("*", (req, res) => {
 	res.status(404);
